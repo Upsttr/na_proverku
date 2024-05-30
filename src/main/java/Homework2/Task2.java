c0b43be897e8b73b14febc5c0ba6cce0bfd039df
upset <upseter@gmail.com>
2024-05-31 00:34:20 +0300

    111

--- src/main/java/Homework2/Task2.java
+++ src/main/java/Homework2/Task2.java
@@ -2,12 +2,12 @@ package Homework2;
 
 public class Task2 {
     public static void main(String[] args) {
-        // Создание объекта типа Flat с указанием значений полей
+
         Flat flat = new Flat(8, 4, 120.5, true, 7500000.0);
         System.out.println("Этаж: " + flat.floor);
         System.out.println("Количество комнат: " + flat.numberOfRooms);
         System.out.println("Площадь: " + flat.area + " кв. м");
-        System.out.println("Наличие балкона: " + (flat.hasBalcony ? "Да" : "Нет"));
+        System.out.println("Наличие балкона: " + (flat.hasBalcony ? "Есть" : "Нет"));
         System.out.println("Цена: " + flat.price + " руб.");
 
         System.out.println();
@@ -16,7 +16,7 @@ public class Task2 {
         System.out.println("Этаж: " + flat2.floor);
         System.out.println("Количество комнат: " + flat2.numberOfRooms);
         System.out.println("Площадь: " + flat2.area + " кв. м");
-        System.out.println("Наличие балкона: " + (flat2.hasBalcony ? "Да" : "Нет"));
+        System.out.println("Наличие балкона: " + (flat2.hasBalcony ? "Есть" : "Нет"));
         System.out.println("Цена: " + flat2.price + " руб.");
     }
 }
